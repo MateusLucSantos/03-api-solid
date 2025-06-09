9486f9868224c6a0af5f99205e65013329e05f3d
MateusLucSantos <mateuslucsantos.dev@gmail.com>
2025-06-09 16:58:36 -0300

    Localizando academias próximas

---
+++ src/use-cases/fetch-nearby-gyms.spec.ts
@@ -0,0 +1,40 @@
+import { beforeEach, describe, expect, it } from 'vitest'
+
+import { InMemoryGymsRepository } from '@/repositories/in-memory/in-memory-gyms-repository'
+import { FetchNearbyGymsUseCase } from './fetch-nearby-gyms'
+
+let gymsRepository: InMemoryGymsRepository
+let sut: FetchNearbyGymsUseCase
+
+describe('Fetch Nearby Gyms Use Case', () => {
+  beforeEach(async () => {
+    gymsRepository = new InMemoryGymsRepository()
+    sut = new FetchNearbyGymsUseCase(gymsRepository)
+  })
+
+  it('should be able to fetch nearby gyms', async () => {
+    await gymsRepository.create({
+      title: 'Near Gym',
+      description: null,
+      phone: null,
+      latitude: -20.0897616,
+      longitude: -45.290002,
+    })
+
+    await gymsRepository.create({
+      title: 'Far Gym',
+      description: null,
+      phone: null,
+      latitude: -20.0257722,
+      longitude: -45.5365403,
+    })
+
+    const { gyms } = await sut.execute({
+      userLatitude: -20.0897616,
+      userLongitude: -45.290002,
+    })
+
+    expect(gyms).toHaveLength(1)
+    expect(gyms).toEqual([expect.objectContaining({ title: 'Near Gym' })])
+  })
+})

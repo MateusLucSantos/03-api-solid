77500981467495be8001a99ea4f06466e70ac911
MateusLucSantos <mateuslucsantos.dev@gmail.com>
2025-06-09 21:27:58 -0300

    validando check-in e validando em até 20 minutos

---
+++ src/use-cases/validate-check-in.spec.ts
@@ -0,0 +1,62 @@
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
+
+import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
+import { ValidateCheckInUseCase } from './validate-check-in'
+import { ResourceNotFoundError } from './ errors/resource-not-found-error'
+
+let checkInRepository: InMemoryCheckInsRepository
+let sut: ValidateCheckInUseCase
+
+describe('Validate Check-in Use Case', () => {
+  beforeEach(async () => {
+    checkInRepository = new InMemoryCheckInsRepository()
+    sut = new ValidateCheckInUseCase(checkInRepository)
+
+    vi.useFakeTimers()
+  })
+
+  afterEach(() => {
+    vi.useRealTimers()
+  })
+
+  it('should be able to validate check in', async () => {
+    const createdCheckIn = await checkInRepository.create({
+      gym_id: 'gym-01',
+      user_id: 'user-01',
+    })
+
+    const { checkIn } = await sut.execute({
+      checkInId: createdCheckIn.id,
+    })
+
+    expect(checkIn.validated_at).toEqual(expect.any(Date))
+    expect(checkInRepository.items[0].validated_at).toEqual(expect.any(Date))
+  })
+
+  it('should be able to validate an inexistent check in', async () => {
+    await expect(
+      sut.execute({
+        checkInId: 'inexistent-check-in-id',
+      }),
+    ).rejects.toBeInstanceOf(ResourceNotFoundError)
+  })
+
+  it('should not be able to validate the check in after 20 minutes of its creation', async () => {
+    vi.setSystemTime(new Date(2025, 0, 20, 8, 0, 0))
+
+    const createdCheckIn = await checkInRepository.create({
+      gym_id: 'gym-01',
+      user_id: 'user-01',
+    })
+
+    const twentyOneMinutesInMs = 1000 * 60 * 21
+
+    vi.advanceTimersByTime(twentyOneMinutesInMs)
+
+    await expect(() =>
+      sut.execute({
+        checkInId: createdCheckIn.id,
+      }),
+    ).rejects.toBeInstanceOf(Error)
+  })
+})
